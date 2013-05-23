21948734b16dae741ab29d00b4b261fe62fb03ee
Scott Golightly <scott_golightly@hotmail.com>
2013-05-23 00:50:29 -0600

    Final Code
    Final code that will turn on a LED when a button is pushed.

---
+++ netduino-pushexternallight/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace netduino_pushexternallight
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            OutputPort led = new OutputPort(Pins.GPIO_PIN_D0, false);
+            InputPort button = new InputPort(Pins.GPIO_PIN_D1, false, Port.ResistorMode.PullUp);
+
+            bool buttonState = false;
+
+            while (true)
+            {
+                buttonState = !button.Read();
+                led.Write(buttonState);
+            }
+        }
+
+    }
+}
